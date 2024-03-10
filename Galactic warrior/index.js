fe6c48c12f94497a17ac1bca0d4202db4ff7abc0
Nagy Bence (WH8L7E) <90764142+Bence9@users.noreply.github.com>
2024-03-10 13:05:02 +0100

    Add files via upload

--- Galactic warrior/index.js
+++ Galactic warrior/index.js
@@ -8,12 +8,39 @@ canvas.height = 600;
 
 const background = new Image();
 background.src = 'images/space.png';
+background.onload = drawCanvas;
 
-const enemyController = new EnemyController(canvas);
+const logoImage = new Image();
+logoImage.src = 'icon.png';
+logoImage.onload = drawCanvas;
 
-function game(){
-    ctx.drawImage(background,0,0,canvas.width,canvas.height);
-    enemyController.draw(ctx);
+function drawCanvas() {
+    if (!background.complete || !logoImage.complete) {
+        return;
+    }
+
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+    ctx.drawImage(logoImage, canvas.width/3 - logoImage.width/2, canvas.height/4 - logoImage.height/2 - 50, 300, 300);
+
+    ctx.font = '30px sans-serif ';
+    ctx.fillStyle = '#39FF14';
+    ctx.textAlign = 'center';
+    ctx.fillText('Press ENTER to play', canvas.width/2, canvas.height/2 + logoImage.height/1 + 25, 500, 500);
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
+            startGame();
+        }
+    });
 }
 
-setInterval(game, 1000/60);
+function startGame() {
+    const enemyController = new EnemyController(canvas);
+
+    function game() {
+        ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+        enemyController.draw(ctx);
+    }
+
+    setInterval(game, 1000/60);
+}
