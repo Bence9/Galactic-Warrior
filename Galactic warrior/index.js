9b2f4e01ea8c1f9dcd88aaec6cf7dfac09453594
Nagy Bence (WH8L7E) <90764142+Bence9@users.noreply.github.com>
2024-07-11 18:38:59 +0200

    Choosable levels, started to write Rules

--- Galactic warrior/index.js
+++ Galactic warrior/index.js
@@ -47,6 +47,111 @@ let life = 3;
 let seconds = 0;
 let gameInterval;
 
+function drawGames() {
+    const buttonImage = new Image();
+    buttonImage.src = "images/ikon/play.png";
+
+    buttonImage.onload = () => {
+        ctx.drawImage(buttonImage, 580, 345, 50, 50);
+    };
+
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2; 
+    ctx.strokeRect(625, 10, 360, 580);
+
+    const buttonImage1 = new Image();
+    buttonImage1.src = "images/ikon/back.png";
+
+    buttonImage1.onload = () => {
+        ctx.drawImage(buttonImage1, 940, 15, 40, 40);
+    };
+
+    drawLevelField(630, 60, 350, 90, 10, 920, 80, 1, "Easy"); // + 105 px (y, playY)
+    drawLevelField(630, 165, 350, 90, 10, 920, 185, 2, "Easy");
+    drawLevelField(630, 270, 350, 90, 10, 920, 290, 3, "Medium");
+    drawLevelField(630, 375, 350, 90, 10, 920, 395, 4, "Medium");
+    drawLevelField(630, 480, 350, 90, 10, 920, 500, 5, "Hard");
+
+    canvas.addEventListener('click', clickHandler);
+
+    function clickHandler(event) {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.x - rect.left;
+        const y = event.y - rect.top;
+
+        if (x >= 920 && x <= 970 && y >= 80 && y <= 110) {
+            startGame();
+            if(isGameOver){
+                restartGame();
+            }
+        }
+        if (x >= 920 && x <= 970 && y >= 185 && y <= 215) {
+            // startGame2()
+        }
+        if (x >= 920 && x <= 970 && y >= 290 && y <= 320) {
+            // startGame3()
+        }
+        if (x >= 920 && x <= 970 && y >= 395 && y <= 425) {
+            // startGame4()
+        }
+        if (x >= 920 && x <= 970 && y >= 500 && y <= 530) {
+            // startGame5()
+        }
+    }
+
+}
+
+function drawLevelField(x, y, width, height, radius, playX, playY, level, challenge) {
+    // Sárga négyzet
+    ctx.strokeStyle = "#FFFF00"; // Sárga
+    ctx.lineWidth = 1;
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.arcTo(x + width, y, x + width, y + radius, radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.arcTo(x + width, y + height, x + width - radius, y + height, radius);
+    ctx.lineTo(x + radius, y + height);
+    ctx.arcTo(x, y + height, x, y + height - radius, radius);
+    ctx.lineTo(x, y + radius);
+    ctx.arcTo(x, y, x + radius, y, radius);
+    ctx.stroke();
+
+    // play gomb
+    const buttonWidth = 50;
+    const buttonHeight = 30;
+    
+    ctx.fillStyle = "#39ff14";
+    ctx.fillRect(playX, playY, buttonWidth, buttonHeight);
+    
+    ctx.fillStyle = "black";
+    ctx.font = "20px sans-serif";
+    ctx.fillText("Play", playX + 25, playY + 20);
+
+    // Szövegmezők
+    ctx.fillStyle = "white";
+    ctx.font = "20px sans-serif";
+    ctx.fillText("Level:" + level, x + 70, y + 30);
+    ctx.font = "16px sans-serif";
+    ctx.fillText("Challenge: " + challenge, x + 70, y + 50);
+    // csillagok, a nehézségi szinthez kötve
+    // money + 50, + 100 ha teljesíti
+
+    level = level % 5;
+
+    if(level == 0){
+        level = 5;
+    }
+
+    const enemyImage = new Image();
+    enemyImage.src = `images/enemy/enemy${level}.png`;
+
+    enemyImage.onload = () => {
+        ctx.drawImage(enemyImage, x + 150, y, 120, 66);
+    };
+
+}
+
 
 function makeButton(text, xPos, yPos, width, height) {
     ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
@@ -88,17 +193,28 @@ function menu() {
     ctx.fillStyle = "#39FF14";
     ctx.textAlign = "center";
 
+//    drawGames();
+
 const clickHandler = (event) => {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    
+
     if (x >= canvas.width/3 && x <= canvas.width/3 + 255 && y >= 350 && y <= 400) {
+        canvas.removeEventListener('mousemove', mouseMoveHandler);
+        drawGames();
+
+/*        if(x >= 920 && x <= 970 && y >= 70 && y <= 100){
+
+        }
+*/
+/*
         startGame();
         canvas.removeEventListener('mousemove', mouseMoveHandler);
         if(isGameOver){
             restartGame();
         }
+*/
     }
     else if(x >= canvas.width/3 && x <= canvas.width/3 + 255 && y >= 400 && y <= 450){
         description.draw();
@@ -163,9 +279,6 @@ function game() {
 
         settings();
 
-//        ctx.fillStyle = "#333333";
-//        ctx.fillRect(0, 0, canvas.width, 30);
-
         ctx.fillStyle = "white";
         ctx.font = "20px sans-serif";
         ctx.fillText("Score: " + enemyController.score, 125, 23);
@@ -178,17 +291,20 @@ function game() {
         ctx.font = "20px sans-serif";
         ctx.fillText("Life: " + life, canvas.width - 125, 23);
 
+        document.addEventListener('keydown', function(event) {
+            if (event.key === 'r' || event.key === 'Escape') {
+                event.preventDefault();
+            }
+        });
+
     } else {
         clearInterval(gameInterval); 
     }
-
 }
 
 // All things that user choose from sound and costumization menu
 function settings(){
-
     player.image.src = costumization.selectedPlayer;
-
     player.draw(ctx);
     enemyController.draw(ctx);
     playerBulletController.draw(ctx);
@@ -201,23 +317,12 @@ function settings(){
     playerBulletController.soundEnabled = sound.soundOn;
     enemyBulletController.soundEnabled = sound.soundOn;
 
-    enemyController.enemyDeathSound.volume = (sound.volume/100);
-    playerBulletController.shootSound.volume = (sound.volume/100);
-    enemyBulletController.shootSound.volume = (sound.volume/100);
+    enemyController.enemyDeathSound.volume = (sound.volume / 100);
+    playerBulletController.shootSound.volume = (sound.volume / 100);
+    enemyBulletController.shootSound.volume = (sound.volume / 100);
 
     playerBulletController.bulletColor = costumization.playerBulletColor;
     enemyBulletController.bulletColor = costumization.enemyBulletColor;
-    
-}
-
-function drawButtonBack() {
-    const buttonImage = new Image();
-    buttonImage.src = "images/ikon/back.png";
-
-    buttonImage.onload = () => {
-        ctx.drawImage(buttonImage, 910, 10, 64, 64);
-    };
-
 }
 
 function displayGameOver(){
@@ -228,17 +333,21 @@ function displayGameOver(){
 
         ctx.fillStyle = "#39ff14";
         ctx.font = "100px Arial";
-        ctx.fillText(text, canvas.width/2 , 250);
+        ctx.fillText(text, canvas.width / 2 , 250);
+
+        ctx.fillStyle = "#39ff14";
+        ctx.font = "25px Arial";
+        let addScore = life * 50; // remaining life added to score
+        enemyController.score += addScore;
+        ctx.fillText("(+ " + addScore + " point added from remaining life)", canvas.width / 2, 340);
+        ctx.font = "30px Arial";
+        ctx.fillText("Total Score: " + enemyController.score, canvas.width / 2, 380);
+        ctx.fillText("Time: " + updateTime(), canvas.width / 2, 420);
 
         ctx.fillStyle = "#39ff14";
         ctx.font = "30px Arial";
-        ctx.fillText("Earned Score: " + enemyController.score, canvas.width/2, 340);
-        ctx.fillText("Time: " + updateTime(), canvas.width/2, 380);
-
-        ctx.fillStyle = "#39ff14";
-        ctx.font = "30px Arial";
-        ctx.fillText("Press [R] to restart game", canvas.width/2, 460);
-        ctx.fillText("Press [Esc] or click [back] to return to menu", canvas.width/2,500);
+        ctx.fillText("Press [R] to restart game", canvas.width / 2, 510);
+        ctx.fillText("Press [Esc] or click [back] to return to menu", canvas.width / 2, 550);
         
         // gomb lenyomás figyelő
         document.addEventListener("keydown", function(event) {
@@ -331,4 +440,14 @@ function formatTime(seconds) {
     const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
     const formattedSeconds = second < 10 ? `0${second}` : `${second}`;
     return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+function drawButtonBack() {
+    const buttonImage = new Image();
+    buttonImage.src = "images/ikon/back.png";
+
+    buttonImage.onload = () => {
+        ctx.drawImage(buttonImage, 910, 10, 64, 64);
+    };
+
 }
