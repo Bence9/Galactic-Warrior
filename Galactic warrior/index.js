166d0e886ddb014445dcd3cae2fe4a64eb67eac5
Nagy Bence (WH8L7E) <90764142+Bence9@users.noreply.github.com>
2024-07-16 16:34:11 +0200

    First animation

--- Galactic warrior/index.js
+++ Galactic warrior/index.js
@@ -28,6 +28,9 @@ const logoImage = new Image();
 logoImage.src = "images/ikon/welcomeIcon.png";
 logoImage.onload = menu;
 
+let animationRunning = true;
+let rubin = 100;
+
 const description = new Description(canvas, ctx, background, menu);
 const sound = new Sound(canvas, ctx, background, menu);
 const costumization = new Costumization(canvas, ctx, menu);
@@ -47,59 +50,53 @@ let life = 3;
 let seconds = 0;
 let gameInterval;
 
-function drawGames() {
-    const buttonImage = new Image();
-    buttonImage.src = "images/ikon/play.png";
 
-    buttonImage.onload = () => {
-        ctx.drawImage(buttonImage, 580, 345, 50, 50);
-    };
+const spriteWidth = 24;
+const spriteHeight = 24;
+const xPos = 170;
+const yPos = 280;
+const scale = 1;
+let frameIndex = 0;
+let count = 0;
 
-    ctx.strokeStyle = "red";
-    ctx.lineWidth = 2; 
-    ctx.strokeRect(625, 10, 360, 580);
+// Ruby sprite betöltése
+const spriteSheet = new Image();
+spriteSheet.src = "images/ikon/ruby.png";
 
-    const buttonImage1 = new Image();
-    buttonImage1.src = "images/ikon/back.png";
+function animate() {
+  ctx.drawImage(
+    spriteSheet,
+    frameIndex * spriteWidth,
+    0,
+    spriteWidth,
+    spriteHeight,
+    xPos,
+    yPos,
+    spriteWidth * scale,
+    spriteHeight * scale
+  );
 
-    buttonImage1.onload = () => {
-        ctx.drawImage(buttonImage1, 940, 15, 40, 40);
-    };
+  count++;
+  if (count >= 100) {
+    frameIndex++;
+    count = 0;
+  }
 
-    drawLevelField(630, 60, 350, 90, 10, 920, 80, 1, "Easy"); // + 105 px (y, playY)
-    drawLevelField(630, 165, 350, 90, 10, 920, 185, 2, "Easy");
-    drawLevelField(630, 270, 350, 90, 10, 920, 290, 3, "Medium");
-    drawLevelField(630, 375, 350, 90, 10, 920, 395, 4, "Medium");
-    drawLevelField(630, 480, 350, 90, 10, 920, 500, 5, "Hard");
-
-    canvas.addEventListener('click', clickHandler);
-
-    function clickHandler(event) {
-        const rect = canvas.getBoundingClientRect();
-        const x = event.x - rect.left;
-        const y = event.y - rect.top;
-
-        if (x >= 920 && x <= 970 && y >= 80 && y <= 110) {
-            startGame();
-            if(isGameOver){
-                restartGame();
-            }
-        }
-        if (x >= 920 && x <= 970 && y >= 185 && y <= 215) {
-            // startGame2()
-        }
-        if (x >= 920 && x <= 970 && y >= 290 && y <= 320) {
-            // startGame3()
-        }
-        if (x >= 920 && x <= 970 && y >= 395 && y <= 425) {
-            // startGame4()
-        }
-        if (x >= 920 && x <= 970 && y >= 500 && y <= 530) {
-            // startGame5()
-        }
-    }
+  if (frameIndex > 6) {
+    frameIndex = 0;
+  }
 
 }
+
+function frame() {
+    if (animationRunning) {
+        ctx.clearRect(background, xPos, yPos, spriteWidth * scale); // Clear animtionzone (xPos, yPos)
+
+        animate();
+      }
+      requestAnimationFrame(frame);
+}
+
 
 function drawLevelField(x, y, width, height, radius, playX, playY, level, challenge) {
     // yellow square
@@ -134,10 +131,9 @@ function drawLevelField(x, y, width, height, radius, playX, playY, level, challe
     ctx.fillText("Level:" + level, x + 70, y + 30);
     ctx.font = "16px sans-serif";
     ctx.fillText("Challenge: " + challenge, x + 70, y + 50);
-    // money + 50, + 100 if plyaer completed level
 
+    // money + 50, + 100 if player completed level
     level = level % 5;
-
     if(level == 0){
         level = 5;
     }
@@ -151,6 +147,94 @@ function drawLevelField(x, y, width, height, radius, playX, playY, level, challe
 
 }
 
+/*
+function drawRubin(){
+    ctx.fillStyle = "white";
+    ctx.font = "22px sans-serif";
+    ctx.fillText("x" + rubin, 140, 300);
+}
+*/
+
+function drawGames() {
+    const buttonImage = new Image();
+    buttonImage.src = "images/ikon/play.png";
+
+    buttonImage.onload = () => {
+        ctx.drawImage(buttonImage, 580, 345, 50, 50);
+    };
+
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 2; 
+    ctx.strokeRect(625, 10, 360, 580);
+
+    const buttonImage1 = new Image();
+    buttonImage1.src = "images/ikon/back.png";
+
+    buttonImage1.onload = () => {
+        ctx.drawImage(buttonImage1, 940, 15, 40, 40);
+    };
+
+    drawLevelField(630, 60, 350, 90, 10, 920, 80, 1, "Easy"); // + 105 px (y, playY)
+    drawLevelField(630, 165, 350, 90, 10, 920, 185, 2, "Easy");
+    drawLevelField(630, 270, 350, 90, 10, 920, 290, 3, "Medium");
+    drawLevelField(630, 375, 350, 90, 10, 920, 395, 4, "Medium");
+    drawLevelField(630, 480, 350, 90, 10, 920, 500, 5, "Hard");
+
+    // Refresh button
+    ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
+    ctx.fillRect(160, 320, 50, 30);
+    ctx.strokeStyle = "red";
+    ctx.strokeRect(160, 320, 50, 30);
+    ctx.fillStyle = "#39ff14";
+    ctx.font = "14px sans-serif";
+    ctx.fillText("Refresh", 185, 340);
+
+    canvas.addEventListener('click', clickHandler);
+
+//    drawRubin();
+
+    function clickHandler(event) {
+        const rect = canvas.getBoundingClientRect();
+        const x = event.x - rect.left;
+        const y = event.y - rect.top;
+
+        if (x >= 920 && x <= 970 && y >= 80 && y <= 110) {
+            startGame();
+            animationRunning = false;
+            if(isGameOver){
+                restartGame();
+            }
+        }
+        if (x >= 920 && x <= 970 && y >= 185 && y <= 215) {
+            // animationRunning = false;
+            // startGame2()
+        }
+        if (x >= 920 && x <= 970 && y >= 290 && y <= 320) {
+            // animationRunning = false;
+            // startGame3()
+        }
+        if (x >= 920 && x <= 970 && y >= 395 && y <= 425) {
+            // animationRunning = false;
+            // startGame4()
+        }
+        if (x >= 920 && x <= 970 && y >= 500 && y <= 530) {
+            // animationRunning = false;
+            // startGame5()
+        }
+        if( x >= 160 && x <= 210 && y >= 320 && y <= 350){
+            animationRunning = true;
+//          rubin += 10;
+//          drawRubin();
+        }
+    }
+
+}
+
+spriteSheet.onload = function() {
+    frame();
+};
+
+frame();
 
 function makeButton(text, xPos, yPos, width, height) {
     ctx.fillStyle = "rgba(0, 0, 0, 0.5)";
@@ -175,7 +259,7 @@ const buttons = [
     { text: "Start Game", x: canvas.width / 3, y: 350, width: 255, height: 50 },
     { text: "Description", x: canvas.width / 3, y: 400, width: 255, height: 50 },
     { text: "Sound", x: canvas.width / 3, y: 450, width: 255, height: 50 },
-    { text: "Customization", x: canvas.width / 3, y: 500, width: 255, height: 50 }
+    { text: "Customization", x: canvas.width / 3, y: 500, width: 255, height: 50 },
 ];
 
 function menu() {
@@ -204,14 +288,17 @@ const clickHandler = (event) => {
     else if(x >= canvas.width/3 && x <= canvas.width/3 + 255 && y >= 400 && y <= 450){
         description.draw();
         canvas.removeEventListener('mousemove', mouseMoveHandler);
+        animationRunning = false;
     }
     else if(x >= canvas.width/3 && x <= canvas.width/3 + 255 && y >= 450 && y <= 500){
         sound.draw();
         canvas.removeEventListener('mousemove', mouseMoveHandler);
+        animationRunning = false;
     }
     else if(x >= canvas.width/3 && x <= canvas.width/3 + 255 && y >= 500 && y <= 550){
         costumization.draw();
         canvas.removeEventListener('mousemove', mouseMoveHandler);
+        animationRunning = false;
     }
 
 };
@@ -358,6 +445,7 @@ function displayGameOver(){
                 restartGame();
             }
             else if (event.key === "Escape"){
+                animationRunning = true;
                 menu();
             }
         });
@@ -369,6 +457,7 @@ function displayGameOver(){
             const y = event.clientY - rect.top;
             
             if (x >= 910 && x <= 974 && y >= 10 && y <= 74) {
+                animationRunning = true;
                 menu();
             }
         };
